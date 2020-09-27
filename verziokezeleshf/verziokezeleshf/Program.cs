7eb31cc82489645701dea4c75faf1d5082b245a7
sarkozilily <sarkozilily@gmail.com>
2020-09-27 20:00:21 +0200

    Bekértem 10 számot és kiszámoltam az átlagot (2.feladat)

---
+++ verziokezeleshf/verziokezeleshf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace verziokezeleshf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            double atlag;
+            int i, x, osszeadas = 0;
+
+            Console.Write("Adjon meg 10 számot! \n");
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("A(z) {0}. szám :", i);
+
+                x = Convert.ToInt32(Console.ReadLine());
+                osszeadas += x;
+            }
+            atlag = osszeadas / 10.0;
+            Console.Write("Az átlaguk : "+ atlag);
+        }
+
+    }
+   }
+
+        
+    
